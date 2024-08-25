0007ad162f546d6ea6ac03065853f7e6d501e89e
JefersonAlves29 <155747099+Jeferson29programador@users.noreply.github.com>
2024-08-25 09:15:55 -0300

    atualizei para ficar mais organizado

--- Exc1.py
+++
@@ -1,4 +0,0 @@
-num1 = int(input("Digite o primeiro numero: "))
-num2 = int(input("Digite o segundo numero: "))
-soma = num1 + num2
-print("O resultado da soma e: ",soma)
--- exc5.py
+++
@@ -1,4 +0,0 @@
-metros = int(input("digite o valor em metros:"))
-cm = metros * 100
-mm = metros * 1000
-print(metros," metros para centimentors e: ",cm ," e para milimetros e: ",mm)
